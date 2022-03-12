6915fdeab99499c824331f4a5aebaaf0a3583c81
Mahilan manuel <101292099+Mahilanmanuel@users.noreply.github.com>
2022-03-12 20:29:14 +0530

    Add files via upload

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports = {
 	name: "messageCreate",
@@ -13,8 +14,27 @@ module.exports = {
 			const m = new MessageEmbed()
 				.setColor("#0099ff")
 				.setDescription(
-					`Hello, I am ${message.client.user.username} and i am a small music bot!\n I'm entirely slash commands based , try doing \`\`\`/help\`\`\``
+					`Hello, I am ${message.client.user.username} and i am Archane Score!\n I'm entirely slash commands based , try doing \`\`\`/help\`\`\``
 				);
+
+if (!message.guild.me.permissions.has(Discord.Permissions.FLAGS.SEND_MESSAGES)) return;
+            if (!message.guild.me.permissions.has(Discord.Permissions.FLAGS.USE_EXTERNAL_EMOJIS))
+                return message.reply({
+                    content: `❌ I am missing the Permission to \`USE_EXTERNAL_EMOJIS\``
+                })
+            if (!message.guild.me.permissions.has(Discord.Permissions.FLAGS.EMBED_LINKS))
+                return message.reply({
+                    content: `❌ I am missing the Permission to \`EMBED_LINKS\``
+                })
+            if (!message.guild.me.permissions.has(Discord.Permissions.FLAGS.ADD_REACTIONS))
+                return message.reply({
+                    embeds: [new MessageEmbed()
+                        .setColor("RED")
+                        .setTitle(`❌ I am missing the Permission to \`ADD_REACTIONS\``)
+                    ]
+                }) 
+
+      
 			return message.reply({ embeds: [m] });
 		}
 	},
--- events/onReady.js
+++ events/onReady.js
@@ -4,38 +4,25 @@ module.exports = {
 	once: true,
 
 	execute(client) {
-		client.user.setActivity("to /help", { type: "LISTENING" });
+		client.user.setActivity(`/help | Mahilan | ${client.guilds.cache.size}`, { type: "LISTENING" });
 
 		console.log(
 			chalk.yellowBright.bold(`
-  ███╗   ███╗██╗   ██╗███████╗██╗ ██████╗ ██████╗                               
-  ████╗ ████║██║   ██║██╔════╝██║██╔════╝██╔═══██╗                              
-  ██╔████╔██║██║   ██║███████╗██║██║     ██║   ██║                              
-  ██║╚██╔╝██║██║   ██║╚════██║██║██║     ██║   ██║                              
-  ██║ ╚═╝ ██║╚██████╔╝███████║██║╚██████╗╚██████╔╝                              
-  ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝ ╚═════╝                               
-                                                                                
-                      ██████╗ ██╗   ██╗    ██╗    ██╗██╗  ██╗██╗██████╗ ██╗     
-                      ██╔══██╗╚██╗ ██╔╝    ██║    ██║██║  ██║██║██╔══██╗██║     
-                      ██████╔╝ ╚████╔╝     ██║ █╗ ██║███████║██║██████╔╝██║     
-                      ██╔══██╗  ╚██╔╝      ██║███╗██║██╔══██║██║██╔══██╗██║     
-                      ██████╔╝   ██║       ╚███╔███╔╝██║  ██║██║██║  ██║███████╗
-                      ╚═════╝    ╚═╝        ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝
-                                                                                
+Archane Score IS ONLINE MADE BY Mahilan                                                     
   `)
 		);
 		console.log(
-			chalk.green.bold(`[Musico] | Logged in as ${client.user.tag}!`)
+			chalk.green.bold(`[Archane Score] | Logged in as ${client.user.tag}!`)
 		);
 		console.log(
-			chalk.yellow.bold(`[Musico] | Servers! ["${client.guilds.cache.size}"]`)
+			chalk.yellow.bold(`[Archane Score] | Servers! ["${client.guilds.cache.size}"]`)
 		);
 		console.log(
-			chalk.red.bold(`[Musico] | Users! ["${client.users.cache.size}"]`)
+			chalk.red.bold(`[HJ OP] | Archane Score Users! ["${client.users.cache.size}"]`)
 		);
 		console.log(
-			chalk.cyan.bold(`[Musico] | Channels! ["${client.channels.cache.size}"]`)
+			chalk.cyan.bold(`[HJ OP]! ["${client.channels.cache.size}"]`)
 		);
-		console.log(chalk.greenBright(`[Musico] | Loaded all (/) commands`));
+		console.log(chalk.greenBright(`[HJ OP] |Archane Score ready (/) commands`));
 	},
 };
